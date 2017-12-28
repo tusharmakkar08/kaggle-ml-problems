9230c9582376cba30ab78e93c79fcab4073af4fe
tusharmakkar08 <tusharmakkar08@gmail.com>
2017-12-28 14:47:29 +0530

    Basic logistic regression made and answer uploaded

--- titanic/survivor.py
+++ titanic/survivor.py
@@ -2,45 +2,71 @@
 import numpy
 import pandas
 
-from sklearn import linear_model, decomposition, datasets
+from sklearn.model_selection import train_test_split, cross_val_score
 
-train_data = pandas.DataFrame.from_csv('train.csv')
+from sklearn import linear_model
 
-y_train = train_data['Survived']
-train_data['Sex'] = train_data['Sex'].astype('category')
-train_data['Sex'] = train_data['Sex'].cat.reorder_categories(['male', 'female'], ordered=True)
-train_data['Sex'] = train_data['Sex'].cat.codes
 
-train_data['Age'] = train_data['Age'].fillna(numpy.median(train_data['Age'].fillna(0)))
+def convert_data(x_var_data):
+    x_var_data['Sex'] = x_var_data['Sex'].astype('category')
+    x_var_data['Sex'] = x_var_data['Sex'].cat.reorder_categories(['male', 'female'], ordered=True)
+    x_var_data['Sex'] = x_var_data['Sex'].cat.codes
 
-train_data['Embarked'] = train_data['Embarked'].astype('category')
-train_data['Embarked'] = train_data['Embarked'].cat.reorder_categories(['C', 'Q', 'S'], ordered=True)
-train_data['Embarked'] = train_data['Embarked'].cat.codes
+    x_var_data['Age'] = x_var_data['Age'].fillna(numpy.median(x_var_data['Age'].fillna(0)))
+    x_var_data['Fare'] = x_var_data['Fare'].fillna(numpy.median(x_var_data['Fare'].fillna(0)))
 
-tickets = []
-for value in train_data['Ticket']:
-    try:
-        int(value)
-        tickets.append(0)
-    except ValueError:
-        tickets.append(1)
-train_data['Ticket'] = tickets
+    x_var_data['Embarked'] = x_var_data['Embarked'].astype('category')
+    x_var_data['Embarked'] = x_var_data['Embarked'].cat.reorder_categories(['C', 'Q', 'S'], ordered=True)
+    x_var_data['Embarked'] = x_var_data['Embarked'].cat.codes
 
-designation = []
-various_designations = ['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Don.', 'Col.', 'Dr.', 'Countess.', 'Jonkheer.',
-                        'Rev.', 'Capt.', 'Mlle.', 'Major.', 'Lady.', 'Mme.', 'Sir.', 'Ms.']
-for value in train_data['Name']:
-    for cnt, i in enumerate(various_designations):
-        if i in value:
-            designation.append(cnt)
-            break
-train_data['Name'] = designation
-train_data['Cabin'] = pandas.isna(train_data['Cabin'])
-train_data.pop('Survived')
-x_train = train_data
+    tickets = []
+    for value in x_var_data['Ticket']:
+        try:
+            int(value)
+            tickets.append(0)
+        except ValueError:
+            tickets.append(1)
+    x_var_data['Ticket'] = tickets
+
+    designation = []
+    various_designations = ['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Don.', 'Col.', 'Dr.', 'Countess.', 'Jonkheer.',
+                            'Rev.', 'Capt.', 'Mlle.', 'Major.', 'Lady.', 'Mme.', 'Sir.', 'Ms.', 'Dona.']
+    for value in x_var_data['Name']:
+        flag_false = True
+        for cnt, i in enumerate(various_designations):
+            if i in value:
+                flag_false = False
+                designation.append(cnt)
+                break
+        if flag_false:
+            designation.append(len(various_designations))
+    x_var_data['Name'] = designation
+    x_var_data['Cabin'] = pandas.isna(x_var_data['Cabin'])
+    return x_var_data
+
+x_data = pandas.DataFrame.from_csv('train.csv')
+x_data = convert_data(x_data)
+y_data = x_data['Survived']
+x_data.pop('Survived')
+
 
 logistic = linear_model.LogisticRegression()
-logistic.fit(x_train, y_train)
 
-print(logistic.score(x_train, y_train))
+# Cross Validation score
+cross_scores = cross_val_score(logistic, x_data, y_data, cv=10)
+print("Accuracy: %0.2f (+/- %0.2f)" % (cross_scores.mean(), cross_scores.std() * 2))
 
+# Normal breaking test size
+X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.1, random_state=0)
+logistic.fit(X_train, y_train)
+print("Training data result %s" % logistic.score(X_train, y_train))
+print("Testing data result %s" % logistic.score(X_test, y_test))
+
+
+x_data_test = pandas.DataFrame.from_csv('test.csv')
+x_data_test = convert_data(x_data_test)
+y_pred = logistic.predict(x_data_test)
+pandas.DataFrame({
+    'PassengerId': x_data_test.index,
+    'Survived': y_pred,
+}).set_index("PassengerId").to_csv('titanic_pred.csv')
